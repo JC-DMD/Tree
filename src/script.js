7bf0b7e1e47ca9f2dd460eb91fc939035bad570f
JC-DMD <140718446+JC-DMD@users.noreply.github.com>
2025-02-24 16:06:52 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -43,7 +43,7 @@ var options = {
                   enabled: true,
                   direction: 'LR', 
                   sortMethod: 'directed',
-                  nodeSpacing: 30,
+                  nodeSpacing: 10,
                   levelSeparation: 400
             }
       	},
